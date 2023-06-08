5419fae0e3818385fc15d27d50ae87a4b7642209
AhmarIftikhar <ahmariftikhar706@gmail.com>
2023-06-08 14:37:03 +0500

    ok

--- src/Components/Reducers/Reducers.js
+++ src/Components/Reducers/Reducers.js
@@ -1,32 +1,31 @@
 const INIT_STATE = {
-    getPaymentTallies: [],
-    paymentTalliesSuccess: "",
-    paymentTalliesError: ""
-  };
-  
-  export default function (state = INIT_STATE, action) {
-    switch (action.type) {
-      case "GET_PAYMENT_TALLIES_INIT":
-        return {
-          ...state,
-          getPaymentTallies: [],
-          paymentTalliesSuccess: "",
-          paymentTalliesError: ""
-        };
-      case "GET_PAYMENT_TALLIES_SUCCESS":
-        return {
-          ...state,
-          getPaymentTallies: action.payload || [],
-          paymentTalliesSuccess: action.payload
-        };
-  
-      case "GET_PAYMENT_TALLIES_FAIL":
-        return {
-          ...state,
-          paymentTalliesError: action.payload
-        };
-      default:
-        return state;
-    }
+  getPaymentTallies: [],
+  paymentTalliesSuccess: "",
+  paymentTalliesError: "",
+};
+
+export default function (state = INIT_STATE, action) {
+  switch (action.type) {
+    case "GET_PAYMENT_TALLIES_INIT":
+      return {
+        ...state,
+        getPaymentTallies: [],
+        paymentTalliesSuccess: "",
+        paymentTalliesError: "",
+      };
+    case "GET_PAYMENT_TALLIES_SUCCESS":
+      return {
+        ...state,
+        getPaymentTallies: action.payload || [],
+        paymentTalliesSuccess: action.payload,
+      };
+
+    case "GET_PAYMENT_TALLIES_FAIL":
+      return {
+        ...state,
+        paymentTalliesError: action.payload,
+      };
+    default:
+      return state;
   }
-  
+}
